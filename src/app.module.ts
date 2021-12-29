69353c3cff1b7bdaf13aed4559a947dda78955e1
ghostlwj <ghostlwj@gmail.com>
2021-12-29 23:32:40 +0800

    feat: mongoose, DatabaseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AuthModule } from './module/auth/auth.module';
+import { DatabaseModule } from './module/database/database.module';
 import { WeatherModule } from './module/weather/weather.module';
 
 @Module({
   controllers: [AppController],
-  imports: [WeatherModule, AuthModule],
+  imports: [WeatherModule, AuthModule, DatabaseModule],
 })
 export class AppModule {}
